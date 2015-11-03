8f55aa18335d5889b8f2a7558d6cc96f18584606
Stella Gloeckner <sgloeckner@pc08.pool.tp1.uni-duesseldorf.de>
2015-11-03 16:02:31 +0100

    hw1

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main() {
+	int n, firstTerm = 1, secondTerm = 1, nextTerm;
+	cout << "Enter the nummer of Term:" << endl;
+	cout << "For N=";
+	cin >> n;
+	
+
+
+	for (int i = 1; i <= n - 2; ++i) {
+		nextTerm = firstTerm + secondTerm;
+		firstTerm = secondTerm;
+		secondTerm = nextTerm;
+	}
+	cout << "f(N)= " <<nextTerm << endl;
+	getch();
+	return 0;
+}
